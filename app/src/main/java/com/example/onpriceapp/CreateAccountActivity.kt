5ad0e27ff932c188e2a8caa22f02fb71561b2a42
isaCarvalho <isabelacarvalhocf@hotmail.com>
2020-04-04 15:23:45 -0300

    Tela de login

---
+++ app/src/main/java/com/example/onpriceapp/CreateAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.example.onpriceapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
--- app/src/main/java/com/example/onpriceapp/StoreLoginActivity.kt
+++ app/src/main/java/com/example/onpriceapp/StoreLoginActivity.kt
@@ -4,20 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
-import androidx.cardview.widget.CardView
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class StoreLoginActivity : AppCompatActivity() {
 
-    var cardStore : CardView? = null
+    var createAccountButton : Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_store_login)
 
-        cardStore = findViewById(R.id.cardStore)
-
-        cardStore!!.setOnClickListener {
-            val intent = Intent(this, StoreLoginActivity::class.java)
+        createAccountButton = findViewById(R.id.createAccountButton)
+        createAccountButton!!.setOnClickListener {
+            val intent = Intent(this, CreateAccountActivity::class.java)
             startActivity(intent)
         }
     }
